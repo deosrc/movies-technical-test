b235e8b14a284b8084660d885d2378f105d5556d
Deosrc <deosrc@g-de.co.uk>
2024-04-26 17:35:17 +0100

    Add database access via entity framework

--- Deosrc.MoviesTechnicalTest.Api/Controllers/MovieController.cs
+++ Deosrc.MoviesTechnicalTest.Api/Controllers/MovieController.cs
@@ -1,32 +1,37 @@
+using Deosrc.MoviesTechnicalTest.Api.DataAccess;
 using Deosrc.MoviesTechnicalTest.Api.Models.Requests;
 using Deosrc.MoviesTechnicalTest.Api.Models.Responses;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Deosrc.MoviesTechnicalTest.Api.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class MovieController(ILogger<MovieController> logger) : ControllerBase
+    public class MovieController(MovieDatabaseContext dbContext, ILogger<MovieController> logger) : ControllerBase
     {
+        private readonly MovieDatabaseContext _dbContext = dbContext;
         private readonly ILogger<MovieController> _logger = logger;
 
         [HttpPost(Name = "Search")]
-        public IEnumerable<MovieResponse> Search(MovieSearchRequest searchRequest)
+        public async Task<IEnumerable<MovieResponse>> Search(MovieSearchRequest searchRequest)
         {
-            return Enumerable.Range(1, 5).Select(index => new MovieResponse
-            {
-                Id = Guid.NewGuid(),
-                Title = $"Movie {index}",
-                Overview = $"Movie {index}",
-                ReleaseDate = "2024-04-26",
-                Popularity = 0,
-                VoteCount = 0,
-                VoteAverage = 0,
-                OriginalLanguage = "en",
-                Genres = [],
-                PosterUrl = ""
-            })
-            .ToArray();
+            return await _dbContext.Movies
+                .Take(10)
+                .Select(m=> new MovieResponse
+                {
+                    Id = m.Id,
+                    Title = "Movie",
+                    Overview = "Movie",
+                    ReleaseDate = "2024-04-26",
+                    Popularity = 0,
+                    VoteCount = 0,
+                    VoteAverage = 0,
+                    OriginalLanguage = "en",
+                    Genres = Array.Empty<string>(),
+                    PosterUrl = ""
+                })
+                .ToListAsync();
         }
     }
 }
---
+++ Deosrc.MoviesTechnicalTest.Api/Entities/Movie.cs
@@ -0,0 +1,7 @@
+namespace Deosrc.MoviesTechnicalTest.Api.Entities
+{
+    public record Movie
+    {
+        public required Guid Id { get; set; }
+    }
+}
