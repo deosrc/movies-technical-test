e017554306ae44e02b27a7a8b542998a20a0d730
Deosrc <deosrc@g-de.co.uk>
2024-04-26 18:29:10 +0100

    Add page info to response

--- Deosrc.MoviesTechnicalTest.Api/Services/Search/EFMovieSearchService.cs
+++ Deosrc.MoviesTechnicalTest.Api/Services/Search/EFMovieSearchService.cs
@@ -9,20 +9,32 @@ namespace Deosrc.MoviesTechnicalTest.Api.Services.Search
         private readonly MovieDatabaseContext _dbContext = dbContext;
         private readonly ILogger<EFMovieSearchService> _logger = logger;
 
-        public async Task<IEnumerable<Movie>> SearchAsync(string title, PagingOptions pagingOptions)
+        public async Task<PagedResult<Movie>> SearchAsync(string title, PagingOptions pagingOptions)
         {
             _logger.LogInformation("Searching for movie with title '{title}'...", title);
 
             var results = await _dbContext.Movies
+                .AsNoTracking()
                 .Where(x => x.Title.ToLower().Contains(title.ToLower()))
                 .OrderByDescending(x => x.Popularity)
                 .Skip((pagingOptions.Page - 1) * pagingOptions.ItemsPerPage)
-                .Take(pagingOptions.ItemsPerPage)
+                .Take(pagingOptions.ItemsPerPage + 1) // Retrieve one more than request to see if there are more results
                 .ToListAsync();
+
+            // Check if there are more results. If there are, reduce the list to the requested amount.
+            var hasMorePages = results.Count > pagingOptions.ItemsPerPage;
+            if (hasMorePages)
+            {
+                results = results.Take(pagingOptions.ItemsPerPage).ToList();
+            }
 
             _logger.LogInformation("Returning {count} movies for title search '{title}'.", results.Count, title);
 
-            return results;
+            return new()
+            {
+                Results = results,
+                Page = new(pagingOptions, hasMorePages)
+            };
         }
     }
 }
