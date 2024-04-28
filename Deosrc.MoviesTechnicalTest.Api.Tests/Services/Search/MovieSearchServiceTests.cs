6787e5b97cd9a4618a6282b30cdca34e11ff96e7
Deosrc <deosrc@g-de.co.uk>
2024-04-28 16:00:57 +0100

    Add paging unit tests

--- Deosrc.MoviesTechnicalTest.Api.Tests/Services/Search/MovieSearchServiceTests.cs
+++ Deosrc.MoviesTechnicalTest.Api.Tests/Services/Search/MovieSearchServiceTests.cs
@@ -15,6 +15,14 @@ namespace Deosrc.MoviesTechnicalTest.Api.Tests.Services.Search
 
         private readonly Movie _searchMatch;
 
+        /// <summary>
+        /// A search string which will match against all results.
+        /// </summary>
+        private const string MatchAllSearch = "";
+
+        /// <summary>
+        /// Paging options a search which will return all matched results as a single page.
+        /// </summary>
         private readonly PagingOptions AllResults = new()
         {
             // This value is not a valid use case, but is used for testing to ensure all matching results are returned.
@@ -57,6 +65,100 @@ namespace Deosrc.MoviesTechnicalTest.Api.Tests.Services.Search
         {
             var results = await _sut.SearchAsync("a b c", AllResults);
             Assert.Empty(results.Results);
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(10)]
+        [InlineData(20)]
+        public async Task SearchAsync_Paging_FirstPageWithMorePages(int pageSize)
+        {
+            _mockRepository
+                .SetupGet(x => x.Movies)
+                .Returns(_fixture.CreateMany<Movie>(100).AsQueryable());
+
+            var results = await _sut.SearchAsync(
+                MatchAllSearch,
+                new PagingOptions()
+                {
+                    ItemsPerPage = pageSize,
+                    Page = 1
+                });
+
+            Assert.Multiple(
+                () => Assert.Equal(pageSize, results.Results.Count()),
+                () => Assert.Equivalent(
+                    new PageInfo()
+                    {
+                        ItemsPerPage = pageSize,
+                        PageNumber = 1,
+                        HasMorePages = true
+                    },
+                    results.Page)
+                );
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(10)]
+        [InlineData(20)]
+        public async Task SearchAsync_Paging_LaterPageWithMorePages(int pageSize)
+        {
+            _mockRepository
+                .SetupGet(x => x.Movies)
+                .Returns(_fixture.CreateMany<Movie>(100).AsQueryable());
+
+            var results = await _sut.SearchAsync(
+                MatchAllSearch,
+                new PagingOptions()
+                {
+                    ItemsPerPage = pageSize,
+                    Page = 2
+                });
+
+            Assert.Multiple(
+                () => Assert.Equal(pageSize, results.Results.Count()),
+                () => Assert.Equivalent(
+                    new PageInfo()
+                    {
+                        ItemsPerPage = pageSize,
+                        PageNumber = 2,
+                        HasMorePages = true
+                    },
+                    results.Page)
+                );
+        }
+
+        [Theory]
+        [InlineData(1, 1, 1)]
+        [InlineData(10, 1, 10)]
+        [InlineData(15, 2, 5)]
+        [InlineData(20, 2, 10)]
+        public async Task SearchAsync_Paging_LastPage(int movieCount, int pageNumber, int expectedResultCount)
+        {
+            _mockRepository
+                .SetupGet(x => x.Movies)
+                .Returns(_fixture.CreateMany<Movie>(movieCount).AsQueryable());
+
+            var results = await _sut.SearchAsync(
+                MatchAllSearch,
+                new PagingOptions()
+                {
+                    ItemsPerPage = 10,
+                    Page = pageNumber
+                });
+
+            Assert.Multiple(
+                () => Assert.Equal(expectedResultCount, results.Results.Count()),
+                () => Assert.Equivalent(
+                    new PageInfo()
+                    {
+                        ItemsPerPage = 10,
+                        PageNumber = pageNumber,
+                        HasMorePages = false
+                    },
+                    results.Page)
+                );
         }
     }
 }
